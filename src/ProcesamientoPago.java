f3f934db14d3b45200091bfc5a2a0b83fdf16541
ffporras <fporras@correo.um.edu.uy>
2024-09-29 16:53:47 -0300

    Procesamiento nuevo

--- src/ProcesamientoPago.java
+++ src/ProcesamientoPago.java
@@ -1,23 +1,41 @@
-import java.util.concurrent.Callable;
 
-public class ProcesamientoPago implements Callable<Boolean> {
-    private Pedido pedido;
+public class ProcesamientoPago implements Runnable{
+    private final Pedido pedido;
 
     public ProcesamientoPago(Pedido pedido) {
         this.pedido = pedido;
     }
     @Override
-    public Boolean call() {
-        // Lógica de procesamiento de pago
-        System.out.println("Processing payment for order: " + pedido.getId());
-        // Simular procesamiento de pago
+    public void run() {
         try {
+            // Verificar si el pedido está empaquetado antes de enviarlo
+            if (pedido.getEstado() != EstadoPedido.PAGO_PENDIENTE) {
+                throw new IllegalStateException("El pedido ya está pago.");
+            }
+            
+            // Lógica de procesamiento de pago
+            System.out.println("Procesamiento de pago para orden: " + pedido.getId());
+            // Simulación del proceso de envío
+            procesarPago(pedido);
+
+            // Cambiar el estado del pedido a ENVIADO
+            pedido.setEstado(EstadoPedido.PAGO_VERIFICADO);
             Thread.sleep(100); // Simulación de tiempo de procesamiento
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            return false;
+            
+        } catch (Exception e) {
+            // Manejo de errores en el envío
+            System.err.println("Error al procesar el procesamiento de pago del pedido: " + e.getMessage());
+
         }
-        pedido.setEstado(EstadoPedido.PAGO_VERIFICADO);
-        return true; // Retornar verdadero si el pago fue exitoso
+    }
+
+    // Método para procesar el envío, puede incluir integración con sistemas externos
+    private void procesarPago(Pedido pedido) {
+        // Simular una operación que toma tiempo
+        try {
+            Thread.sleep(2000); // Simula tiempo de procesamiento de envío
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Reestablecer el estado de interrupción
+        }
     }
 }
